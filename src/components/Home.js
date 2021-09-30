b4b073c35a497d93f09675cf49cb35b3bc75156e
eoin-barr <eoin.barr@ucdconnect.ie>
2021-09-30 16:19:20 +0100

    Changes css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,17 @@ function Home() {
       <div className="gif-container">
         <img src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1632954345/portfolio-website/eoin-gif-2_x8zcpa.gif" />
       </div>
+
+
+      <div className=" flex-row title-text">
+        <div>
+          <img />
+        </div>
+        <div>
+          <h2></h2>
+          <p>Here we go and there it was</p>
+        </div>
+      </div>
     </>
   )
 }
