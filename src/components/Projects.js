664afa81363a31a0dd01d283aebef3c977760cd3
eoin-barr <eoin.barr@ucdconnect.ie>
2021-10-05 18:14:00 +0100

    Animation added to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,9 @@ function Projects() {
     <>
       <div id="projects" className="flexi-row py-4">
         <a target="_blank" rel="noreferrer" href="https://painterest.netlify.app/" alt="Painterest" className="proj-img-cont">
-          <img className="proj-img" src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1633015751/portfolio-website/Screenshot_2021-09-30_at_16.27.55_gmiqdc.png" />
+          <img data-aos="fade-right" className="proj-img" src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1633015751/portfolio-website/Screenshot_2021-09-30_at_16.27.55_gmiqdc.png" />
         </a>
-        <div className="proj-desc-cont">
+        <div data-aos="fade-left" className="proj-desc-cont">
           <h2 className="text-align-center">Painterest (Pinterest Clone)</h2>
           <p className="text-align-center para">Create a user, upload paintings, follow and message other users. <br /><p className="no-m-para">Username: Guest &nbsp;&nbsp;&nbsp; Password: pass</p></p>
           <div className="flexi-row-2">
@@ -17,9 +17,9 @@ function Projects() {
       </div>
       <div className="flexi-row py-4">
         <a target="_blank" rel="noreferrer" href="https://airstudio-project.netlify.app/" alt="AiStudio" className="proj-img-cont">
-          <img className="proj-img" src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1633015744/portfolio-website/Screenshot_2021-09-30_at_16.28.16_adbkxe.png" />
+          <img data-aos="fade-right" className="proj-img" src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1633015744/portfolio-website/Screenshot_2021-09-30_at_16.28.16_adbkxe.png" />
         </a>
-        <div className="proj-desc-cont">
+        <div data-aos="fade-left" className="proj-desc-cont">
           <h2 className="text-align-center">AirStudio (Airbnb Clone)</h2>
           <p className="text-align-center para">View, create, edit and book recording studios.<br /><p className="no-m-para">Email: guest@email.com &nbsp;&nbsp;&nbsp; Password: password</p></p>
           <div className="flexi-row-2">
