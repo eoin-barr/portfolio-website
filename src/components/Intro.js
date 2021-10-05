7d0e2a3a988d3d4a9074bdeb015fc186adbc0155
eoin-barr <eoin.barr@ucdconnect.ie>
2021-10-05 18:00:31 +0100

    Animation added to intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@
 function Intro() {
   return (
     <div className="gif-container">
-      <img src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1632954345/portfolio-website/eoin-gif-2_x8zcpa.gif" />
+      <img className="animate-intro" src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1632954345/portfolio-website/eoin-gif-2_x8zcpa.gif" />
     </div>
   )
 }
