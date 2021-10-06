6ee449a509cdeb3633bfd051ae1290cdbd799886
eoin-barr <eoin.barr@ucdconnect.ie>
2021-10-06 21:29:41 +0100

    Initialised arrow icon

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,7 @@ function Intro() {
   return (
     <div className="gif-container">
       <img className="animate-intro" src="https://res.cloudinary.com/dk0r9bcxy/image/upload/v1632954345/portfolio-website/eoin-gif-2_x8zcpa.gif" />
+      {/* <i className="fas fa-arrow-alt-circle-down fa-10x white-bg"></i> */}
     </div>
   )
 }
